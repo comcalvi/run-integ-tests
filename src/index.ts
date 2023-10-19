bf10b7f278ba02c900fc38a1ab18c18861451393
Calvin Combs <comcalvi@amazon.com>
2023-10-19 16:28:39 -0700

    working

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
+//import { execSync } from 'child_process';
 import * as https from 'https';
 import * as core from '@actions/core';
+
 //import * as github from '@actions/github';
 
 
-const RUN_TEST_SUITE_ENDPOINT = 'https://hmc71to6j7.execute-api.us-east-1.amazonaws.com/prod/';
+const RUN_TEST_SUITE_ENDPOINT = 'hmc71to6j7.execute-api.us-east-1.amazonaws.com';
 
 async function run() {
-  const prNumber: string = core.getInput('pr-number', { required: true });
-  const commitSha: string = core.getInput('commit-sha', { required: true });
+  //const prNumber: string = core.getInput('pr-number', { required: true });
+  //const commitSha: string = core.getInput('commit-sha', { required: true });
+
+  const prNumber = '0';
+  const commitSha = 'aws-cdk.zip';
 
   await runTestSuite(prNumber, commitSha);
 
@@ -24,11 +29,23 @@ run().catch(error => {
  * runs a test suite. Will block until the test suite has been started
  */
 async function runTestSuite(prNumber: string, commitSha: string) {
+  let body = '';
+
   const req = https.request({
     host: RUN_TEST_SUITE_ENDPOINT,
     method: 'POST',
+    path: '/prod',
+    headers: {
+      'Content-Type': 'application/json',
+    },
   }, (response) => {
-    console.log(response);
+    response.on('data', (chunk) => {
+      body += chunk;
+    });
+
+    response.on('end', () => {
+      console.log(body);
+    });
   });
 
   req.write(JSON.stringify({
@@ -37,6 +54,10 @@ async function runTestSuite(prNumber: string, commitSha: string) {
   }));
 
   req.end();
+
+  //execSync(`curl https://hmc71to6j7.execute-api.us-east-1.amazonaws.com/prod/ --data-raw '{"prNumber": ${prNumber}, "commitSha": ${commitSha} }'`, {
+  //  stdio: 'inherit',
+  //});
 }
 
 /**
